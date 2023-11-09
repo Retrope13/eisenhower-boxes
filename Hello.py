1dbb8ad9c3fbc6079c58502fddaf3b02e52e76d6
Samuel Mckay <89430082+Retrope13@users.noreply.github.com>
2023-11-09 15:15:02 -0700

    laying ground work for adding text boxes that can close

--- Hello.py
+++ Hello.py
@@ -15,6 +15,7 @@
 import streamlit as st
 from streamlit.logger import get_logger
 from streamlit_elements import elements, mui, html
+import streamlit.components.v1 as components
 import os
 
 LOGGER = get_logger(__name__)
@@ -40,6 +41,7 @@ def run():
         dashboard.Item("third_item", 4, 4, 4, 3),
         dashboard.Item("fourth_item", 0, 8, 4, 3),
         dashboard.Item("fifth_item", 8, 0, 4, 6),
+        dashboard.Item("textItem", 0, 0, 7, 3),
     ]
     
     
@@ -59,12 +61,20 @@ def run():
         st.markdown(f'<div style="position: relative; left: {event["pageX"]}px; top: {event["pageY"]}px;">Element: {new_element}</div>',
         unsafe_allow_html=True)
 
+
+    def closeTextBox(event):
+        print("closing textBox")
+        print(event)
+
     with dashboard.Grid(layout, onLayoutChange=handle_layout_change):
         mui.Paper("Do It", key="first_item", sx={"color":"black", "bgcolor": "#B3FFB3", "alignContent":"center"}, onDoubleClick=handle_text_create) ##?onDoubleClick to create a new paper? maybe button
         mui.Paper("Plan It", key="second_item", sx={"color":"black", "bgcolor": "#FBFFB3",})
         mui.Paper("Delegate It", key="third_item", sx={"color":"black", "bgcolor": "#B3F6FF",})
         mui.Paper("Drop It", key="fourth_item", sx={"color":"black", "bgcolor": "#FFB9B3",})
-        mui.Paper("Done It", key="fifth_item", sx={"color":"black", "bgcolor": "#B3FFB3",})
+        mui.Paper("Done It", key="fifth_item", sx={"position":"relative", "color":"black", "bgcolor": "#B3FFB3", "zIndex": "0"})
+        with dashboard.Grid(layout, onLayoutChange=handle_layout_change):
+            with mui.Paper(sx={"zIndex": "1"}):
+                mui.Button(sx={"bgcolor": "#ff4747", "borderRadius": "100%", "size": "10px"}, onClick=closeTextBox)
   
 
 
